f8826501e4d2f0e123fbb11cd59cdd7e5be54214
Divyam <167672901+professor-03@users.noreply.github.com>
2025-01-07 00:51:49 +0530

    Create integerarithematic.py

---
+++ python-simple/examples/integerarithematic.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 5
+result = num1 // num2
+print("integer Division:", result)
+result2 = num1 % num2
+print("remainder:", result2)
+result3 = abs(-5)
+print("absolute value:", result3)
