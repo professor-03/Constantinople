ad2c0d53424ea2c0f97dc3114623a0419b4b4bbd
Divyam <167672901+professor-03@users.noreply.github.com>
2025-01-07 02:02:39 +0530

    Create integerarithematicc.py

---
+++ python-simple/examples/integerarithematicc.py
@@ -0,0 +1,11 @@
+#This program is unlike the float one, in this all the arithematica calculations involving integer values wii be executed
+num1 = 10 
+num2 = 4 
+result = num1 // num2 
+# // this sign is used to perform division when we have two integer values for the operation 
+print("Integer Division:", result)
+result2 = num1 % num2
+print("The remainder operation calculated as:", result2)
+result3 = abs(-8)
+print("Absolute value will be:", result3)
+
