62eed82cd9a148e1ccce12fd58c26c6d2527b0e2
Divyam <167672901+professor-03@users.noreply.github.com>
2025-01-07 00:13:16 +0530

    Create arithematic.py

---
+++ python-simple/examples/arithematic.py
@@ -0,0 +1,14 @@
+#Float variables 
+num1 = 5.0 
+num2 = 2.5 
+result = num1 + num2
+print("Addition:", result)
+result2 = num1 - num2
+print("Substraction:", result2)
+result3 = num1/num2
+print("Division:", result3)
+result4 = num1 * num2
+print("Multiplication:", result4)
+#Now we are gonna try the rounding off function which can round off the value to any number of entered places 
+value = round(3.145268745955,3) #rounds of the entered number to 3 decimal places 
+print("Rounded:", value)
